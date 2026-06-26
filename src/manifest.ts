349c96fd68fe9eeb9ef245ac6493c6bcd6f515b7
deechael <DeeChael@outlook.com>
2026-06-26 18:52:36 +0800

    feat: add more aspect ratios for PriestessQ wallpapers

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,6 +3,59 @@ import type { WallpaperCollection } from './types';
 
 const manifest: WallpaperCollection[] = [
   {
+    "id": "InfiniteAnon",
+    "name": "无尽爱音",
+    "tags": [
+      "千早爱音",
+      "BanG Dream",
+      "myGO"
+    ],
+    "wallpapers": [
+      {
+        "ratio": "16:9",
+        "file": "InfiniteAnon/16_9_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "16:10",
+        "file": "InfiniteAnon/16_10_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "4:3",
+        "file": "InfiniteAnon/4_3_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "3:2",
+        "file": "InfiniteAnon/3_2_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "9:16",
+        "file": "InfiniteAnon/9_16_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "10:16",
+        "file": "InfiniteAnon/10_16_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "9:20",
+        "file": "InfiniteAnon/9_20_4k.png",
+        "message": ""
+      }
+    ],
+    "arts": [
+      {
+        "url": "https://www.bilibili.com/video/BV1Y4xneKEWp",
+        "title": "无尽爱音！出来！！壁纸",
+        "author": "xe08"
+      }
+    ]
+  },
+  {
     "id": "PriestessQ",
     "name": "Q版 普瑞塞斯",
     "tags": [
@@ -13,17 +66,37 @@ const manifest: WallpaperCollection[] = [
       {
         "ratio": "16:9",
         "file": "PriestessQ/16_9_4k.png",
-        "message": "16:9"
+        "message": ""
       },
       {
         "ratio": "16:10",
         "file": "PriestessQ/16_10_4k.png",
-        "message": "16:10"
+        "message": ""
+      },
+      {
+        "ratio": "4:3",
+        "file": "PriestessQ/4_3_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "3:2",
+        "file": "PriestessQ/3_2_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "10:16",
+        "file": "PriestessQ/10_16_4k.png",
+        "message": ""
+      },
+      {
+        "ratio": "9:16",
+        "file": "PriestessQ/9_16_4k.png",
+        "message": ""
       },
       {
         "ratio": "9:20",
         "file": "PriestessQ/9_20_4k.png",
-        "message": "9:20"
+        "message": ""
       }
     ],
     "arts": [
@@ -31,59 +104,6 @@ const manifest: WallpaperCollection[] = [
         "url": "https://www.pixiv.net/artworks/144633725",
         "title": "普瑞塞斯",
         "author": "二象性冰淇淋"
-      }
-    ]
-  },
-  {
-    "id": "sample",
-    "name": "示例壁纸",
-    "tags": [
-      "示例",
-      "风景",
-      "自然"
-    ],
-    "wallpapers": [
-      {
-        "ratio": "16:9",
-        "file": "sample/16-9.jpg",
-        "message": "16:9 桌面壁纸"
-      },
-      {
-        "ratio": "16:10",
-        "file": "sample/16-10.jpg",
-        "message": "16:10 桌面壁纸"
-      },
-      {
-        "ratio": "4:3",
-        "file": "sample/4-3.jpg",
-        "message": "4:3 壁纸"
-      },
-      {
-        "ratio": "3:2",
-        "file": "sample/3-2.jpg",
-        "message": "3:2 壁纸"
-      },
-      {
-        "ratio": "10:16",
-        "file": "sample/10-16.jpg",
-        "message": "10:16 竖屏壁纸"
-      },
-      {
-        "ratio": "9:16",
-        "file": "sample/9-16.jpg",
-        "message": "9:16 竖屏壁纸"
-      },
-      {
-        "ratio": "9:20",
-        "file": "sample/9-20.jpg",
-        "message": "9:20 竖屏壁纸"
-      }
-    ],
-    "arts": [
-      {
-        "url": "https://example.com/artwork1",
-        "title": "示例作品 1",
-        "author": "示例作者"
       }
     ]
   }
